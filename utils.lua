95df362d6e9ff7289565ea66f828874ae882d6b9
mcdelaney <mcdelaney@gmail.com>
2020-11-05 15:46:03 -0700

    revert statics

--- utils.lua
+++ utils.lua
@@ -316,31 +316,29 @@ local function removeUnit (unitName, smoke)
         -- stc.InitDead = true
         -- stc.InitStaticCoordinate = unitPoint
         -- stc:Spawn()
-
-        -- mist.dynAddStatic(
-        --   {
-        --     type = unit_type,
-        --     country = unit_country,
-        --     category = unit_cat,
-        --     x = unitPoint:GetVec2().x,
-        --     y = unitPoint:GetVec2().y,
-        --     groupName = unit:GetGroup():GetName(),
-        --     groupId = unit:GetGroup():GetID(),
-        --     name = "dead"..unitName,
-        --     clone = false,
-        --     dead = true,
-        --     heading = unit:GetHeading(),
-        --     }
-        -- )
         -- stc:SpawnFromCoordinate(unitPoint)
+
+        mist.dynAddStatic(
+          {
+            type = unit_type,
+            country = unit_country,
+            category = unit_cat,
+            x = unitPoint:GetVec2().x,
+            y = unitPoint:GetVec2().y,
+            groupName = unit:GetGroup():GetName(),
+            groupId = unit:GetGroup():GetID(),
+            name = "dead"..unitName,
+            clone = false,
+            dead = true,
+            heading = unit:GetHeading(),
+            }
+        )
 
         env.info("Dead static spawn successful...")
       else
         log("Could not create static unit: "..unit_type.." - "..unit_cat)
       end
-      -- if smoke==true then
-      --   unitPoint:BigSmokeSmall(0.75)
-      -- end
+
     end
   end
 end
