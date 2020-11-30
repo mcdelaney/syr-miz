87897e13cdfb101f0f220e0fad9738c121f5ad6c
mcdelaney <mcdelaney@gmail.com>
2020-11-30 10:54:22 -0700

    Add hatay hercules

--- ctld.lua
+++ ctld.lua
@@ -242,6 +242,7 @@ ctld.transportPilotNames = {
 
     "Aleppo-Hercules-1",
     "Incirlik-Hercules-1",
+    "Hatay-Hercules-1",
     "Bassel Al-Assad-Hercules-1",
     "Ramat David-Hercules-1",
     "Beirut-Rafic Hariri-Hercules-1",
