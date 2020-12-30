5da24b872ae21b54996ddc07326b0d00543a8c75
mcdelaney <mcdelaney@gmail.com>
2020-12-30 14:26:11 -0700

    fix name

--- main.lua
+++ main.lua
@@ -98,9 +98,9 @@ local _NumAirbaseDefenders = 1
 
 -- AIRBOSS
 -- "CVN-71", "Stennis"
-local airbossStennis=AIRBOSS:New("CVN-73", "CVN-73")
-airbossStennis:SetAutoSave("Scripts/syr-miz/traps/", "CVN-73")
-airbossStennis:Start()
+local airbossCVN73 = AIRBOSS:New("CVN-73", "CVN-73")
+airbossCVN73:SetAutoSave("Scripts/syr-miz/traps/", "CVN-73")
+airbossCVN73:Start()
 
 
 local function setBaseRed(baseName, init_ground)
